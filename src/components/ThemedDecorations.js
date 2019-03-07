00350a163ec5b0338b6205802288ac42248c7ae2
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-03-07 19:31:29 +0000

    Automatically backed up by Learn

--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,11 @@
-// Code ThemedDecoration Component Here
+import React from 'react' ; 
+
+export default class ThemedDecorations extends React.Component {
+  var theme = this.props.theme ;
+  
+  render() {
+    return (
+      
+    )
+  }
+}
