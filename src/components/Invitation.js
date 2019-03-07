d636344c7fc1d47cc6657ead60befc64cd72b19c
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2019-03-07 19:29:25 +0000

    Automatically backed up by Learn

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -5,8 +5,8 @@ export default class Invitation extends React.Component {
   render() {
     return (
       <div>
-      <h1> {"You've been invited!"} </h1>
-      {this.props.children}
+        <h1> {"You've been invited!"} </h1>
+        {this.props.children}
       </div>
     )
   }
